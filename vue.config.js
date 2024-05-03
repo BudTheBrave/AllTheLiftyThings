d23ef75763e2af3d4539c8c412beec79ad417f3c
BudTheBrave <brendan@marshmob.com>
2024-05-02 18:53:45 -0600

    adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/AllTheLiftyThings/'
-    : '/',
+  publicPath: '/AllTheLiftyThings/',
   transpileDependencies: true,
 })
 
