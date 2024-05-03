dbe31d3bf4eae0f12a620f9c8d86a24dd7bfd6e1
BudTheBrave <brendan@marshmob.com>
2024-05-02 18:57:32 -0600

    vue config update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/AllTheLiftyThings/',
-  transpileDependencies: true,
+  transpileDependencies: true
 })
 
